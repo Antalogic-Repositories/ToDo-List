01b2c59b5aa4852693e5103787c1abf520fb4888
Ekaterina <ekaterinan112@gmail.com>
2020-12-28 18:08:30 +0300

    Updates

---
+++ precache-manifest.de7c0aa11638836cc11de10dd756d374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28fef2bd7ec390b0371f7906a791e55a",
+    "url": "/it-incubator-todolist-ts/index.html"
+  },
+  {
+    "revision": "f67ca197e5a0b65c3c05",
+    "url": "/it-incubator-todolist-ts/static/css/main.f9609cda.chunk.css"
+  },
+  {
+    "revision": "73426df1a0b0ab669d01",
+    "url": "/it-incubator-todolist-ts/static/js/2.aa9e72d8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/it-incubator-todolist-ts/static/js/2.aa9e72d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f67ca197e5a0b65c3c05",
+    "url": "/it-incubator-todolist-ts/static/js/main.9831814a.chunk.js"
+  },
+  {
+    "revision": "1ab7487ce92f6fb728b6",
+    "url": "/it-incubator-todolist-ts/static/js/runtime-main.f8216a61.js"
+  }
+]);
